ac167190beb6b24410e5600254e4db39240a7a70
koskos1986 <ekosopletkin@yandex.ru>
2020-10-11 21:54:09 +0300

    Добавляет module3-task2

--- js/main.js
+++ js/main.js
@@ -52,22 +52,25 @@ const MIN_PIN_X = 0;
 const MAX_PIN_X = 1200;
 const MIN_PIN_Y = 130;
 const MAX_PIN_Y = 630;
-
 // Положение центра острия метки
 const INDENT_FOR_PIN_EDGE_X = 25;
 const INDENT_FOR_PIN_EDGE_Y = 35;
-
 // функции для получения случайных элементов
 const getRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const getRandomItemFromArray = (listOfItems) => {
-  return Math.floor(Math.random() * listOfItems.length);
+const getRandomItemFromArray = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
 };
+
+const getSeveralItemsFromArray = (array) => array.slice(0, getRandomNumber(0, array.length - 1));
 // Координаты
 const getPinCoordinates = () => {
-  let getPinCoordinatesArray = [getRandomNumber(MIN_PIN_X, MAX_PIN_X), getRandomNumber(MIN_PIN_Y, MAX_PIN_Y)];
+  let getPinCoordinatesArray = {
+    x: getRandomNumber(MIN_PIN_X, MAX_PIN_X),
+    y: getRandomNumber(MIN_PIN_Y, MAX_PIN_Y)
+  };
   return getPinCoordinatesArray;
 };
 
@@ -75,30 +78,30 @@ const getPinCoordinates = () => {
 const getMockAds = (count) => {
   const ads = [];
 
-  for (let i = 1; i <= count; i++) {
-    const pinLocation = getPinCoordinates(i);
+  for (let i = 0; i < count; i++) {
+    const pinLocation = getPinCoordinates();
     ads.push(
         {
           author: {
-            avatar: `img/avatars/user0${i}.png`
+            avatar: `img/avatars/user0${i + 1}.png`
           },
           offer: {
             title: getRandomItemFromArray(OFFER_TITLE),
-            address: pinLocation,
+            address: pinLocation.x + ` ` + pinLocation.y,
             price: getRandomNumber(MIN_PRICE, MAX_PRICE),
             type: getRandomItemFromArray(TYPE),
             rooms: getRandomNumber(MIN_ROOMS, MAX_ROOMS),
             guests: getRandomNumber(MIN_GUESTS, MAX_GUESTS),
             checkin: getRandomItemFromArray(CHECK_IN_CHECK_OUT),
             checkout: getRandomItemFromArray(CHECK_IN_CHECK_OUT),
-            features: getRandomItemFromArray(FEATURES),
+            features: getSeveralItemsFromArray(FEATURES),
             description: getRandomItemFromArray(DESCRIPTION_ROOM),
-            photos: getRandomItemFromArray(PHOTOS)
+            photos: getSeveralItemsFromArray(PHOTOS)
           },
 
           location: {
-            x: pinLocation[0],
-            y: pinLocation[1],
+            x: pinLocation.x,
+            y: pinLocation.y,
           }
         }
     );
@@ -108,7 +111,6 @@ const getMockAds = (count) => {
 
 // переключает карту в активное состояние
 const mapElement = document.querySelector(`.map`);
-mapElement.classList.remove(`map--faded`);
 
 // подготовка разметки для создания метки
 const mapPinElement = mapElement.querySelector(`.map__pins`);
@@ -142,5 +144,112 @@ const renderPins = () => {
 
   addFragment(pinsNodeFragment);
 };
+// m3-t2
+const generateAdCard = (ad) => {
+  const cardTemplate = document.querySelector(`#card`).content.querySelector(`.map__card`);
+  const cardElement = cardTemplate.cloneNode(true);
 
+  const cardTitle = cardElement.querySelector(`.popup__title`);
+  if (ad.offer.title) {
+    cardTitle.textContent = ad.offer.title;
+  } else {
+    cardTitle.remove();
+  }
+
+  const cardAdress = cardElement.querySelector(`.popup__text--address`);
+  if (ad.offer.address) {
+    cardAdress.textContent = ad.offer.address;
+  } else {
+    cardAdress.remove();
+  }
+
+  const cardPrice = cardElement.querySelector(`.popup__text--price`);
+  if (ad.offer.price) {
+    cardPrice.textContent = ad.offer.price + `₽/ночь`;
+  } else {
+    cardPrice.remove();
+  }
+
+  const cardType = cardElement.querySelector(`.popup__type`);
+  if (ad.offer.type) {
+    switch (ad.offer.type) {
+      case `flat`:
+        cardType.textContent = `Квартира`;
+        break;
+      case `bungalow`:
+        cardType.textContent = `Бунгало`;
+        break;
+      case `house`:
+        cardType.textContent = `Дом`;
+        break;
+      case `palace`:
+        cardType.textContent = `Дворец`;
+        break;
+      default:
+        cardType.textContent = `Неверный тип размещения`;
+        break;
+    }
+  } else {
+    cardType.remove();
+  }
+
+  const cardCapacity = cardElement.querySelector(`.popup__text--capacity`);
+  if (ad.offer.rooms && ad.offer.guests) {
+    cardCapacity.textContent = `${ad.offer.rooms} комнаты для ${ad.offer.guests} гостей`;
+  } else {
+    cardCapacity.remove();
+  }
+
+  const cardCheckinInOut = cardElement.querySelector(`.popup__text--time`);
+  if (ad.offer.checkin && ad.offer.checkout) {
+    cardCheckinInOut.textContent = `Заезд после ${ad.offer.checkin}, выезд\u00A0до ${ad.offer.checkout}`;
+  } else {
+    cardCheckinInOut.remove();
+  }
+
+  const cardFeatures = cardElement.querySelector(`.popup__features`);
+  if (ad.offer.features) {
+    cardFeatures.innerHTML = ``;
+    ad.offer.features.forEach((feature) => {
+      const featureElement = document.createElement(`li`);
+      featureElement.classList.add(`popup__feature`, `popup__feature--${feature}`);
+      cardFeatures.append(featureElement);
+    });
+  } else {
+    cardFeatures.remove();
+  }
+
+  const cardDescription = cardElement.querySelector(`.popup__description`);
+  if (ad.offer.description) {
+    cardDescription.textContent = ad.offer.description;
+  } else {
+    cardDescription.remove();
+  }
+
+  const cardPhotos = cardElement.querySelector(`.popup__photos`);
+  const photoTemplate = cardPhotos.querySelector(`.popup__photo`);
+  if (ad.offer.photos) {
+    cardPhotos.innerHTML = ``;
+    ad.offer.photos.forEach((photo) => {
+      const photoElement = photoTemplate.cloneNode(true);
+      photoElement.src = photo;
+      cardPhotos.append(photoElement);
+    });
+  } else {
+    cardPhotos.remove();
+  }
+
+  const cardAvatar = cardElement.querySelector(`.popup__avatar`);
+  if (ad.author.avatar) {
+    cardAvatar.src = ad.author.avatar;
+  } else {
+    cardAvatar.remove();
+  }
+
+  return cardElement;
+};
+
+const mapFilterContainer = document.querySelector(`.map__filters-container`);
+mapElement.classList.remove(`map--faded`);
+mapElement.insertBefore(generateAdCard(pinsArray[0]), mapFilterContainer);
 renderPins();
